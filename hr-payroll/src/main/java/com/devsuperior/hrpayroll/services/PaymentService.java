b283290fc247eedb81e29409d291820cada25fba
Julio Rocha <juliogabrielmr@gmail.com>
2023-02-14 21:43:09 -0300

    hr-payroll com feignclient

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,25 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclient.WorkerFeignclient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignclient workerFeignclient;
+
     public Payment getPayment(Long workerId, int daysWorked){
-        return new Payment("Bob", 200.0, daysWorked);
+
+        Worker worker = workerFeignclient.findWorkerById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), daysWorked);
     }
 }
