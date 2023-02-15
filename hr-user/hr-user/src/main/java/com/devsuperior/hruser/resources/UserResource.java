e8d37e3f51cc2f2ba8afcc365cffe5b8e7885880
Julio Rocha <juliogabrielmr@gmail.com>
2023-02-15 11:33:05 -0300

    Project hr-oauth created

---
+++ hr-user/hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.devsuperior.hruser.resources;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User obj = userRepository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findUserById(@RequestParam String email){
+        User obj = userRepository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+}
