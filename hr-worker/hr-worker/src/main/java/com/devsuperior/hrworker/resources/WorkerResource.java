468b0772a737f80a1bcf27523c35c21e31b2b1bc
Julio Rocha <juliogabrielmr@gmail.com>
2023-02-15 10:48:23 -0300

    project hr-user created

--- hr-worker/hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
@@ -26,6 +29,16 @@ public class WorkerResource {
 
     @Autowired
     private Environment environment;
+
+
+    @Value("${test.config}")
+    private String testeConfig;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+       logger.info("CONFIG = "+ testeConfig);
+       return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
